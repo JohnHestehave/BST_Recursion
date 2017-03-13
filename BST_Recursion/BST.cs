5b341f378be3ee04e17f988a30feba35050b8e4f
John H <power_mand@hotmail.com>
2017-03-13 11:09:04 +0100

    Add project files.

---
+++ BST_Recursion/BST.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BST_Recursion
+{
+	public class BST
+	{
+		public BSTNode root;
+
+		public int count;
+
+		public void Insert(IComparable data)
+		{
+			if (root == null)
+			{
+				root = new BSTNode();
+				root.data = data;
+				count = 1;
+			}
+			else
+			{
+				BSTNode node = root;
+				while (node != null)
+				{
+					if (data.CompareTo(node.data) < 0)
+					{
+						if (node.left == null)
+						{
+							node.left = new BSTNode();
+							node.left.data = data;
+							count++;
+							break;
+						}
+						node = node.left;
+					}
+					else
+					{
+						if (node.right == null)
+						{
+							node.right = new BSTNode();
+							node.right.data = data;
+							count++;
+							break;
+						}
+						node = node.right;
+					}
+				}
+
+			}
+		}
+
+		public void InsertRecursively(IComparable data)
+		{
+			InsertRecursively(ref root, data);
+		}
+		public void InsertRecursively(ref BSTNode n, IComparable data)
+		{
+			if(n == null)
+			{
+				n = new BSTNode();
+				n.data = data;
+			}else
+			{
+				if (data.CompareTo(n.data) < 0)
+				{
+					InsertRecursively(ref n.left, data);
+				}else
+				{
+					InsertRecursively(ref n.right, data);
+				}
+			}
+		}
+
+		public bool Search(IComparable data)
+		{
+			BSTNode node = root;
+			while (node != null)
+			{
+				if (data.CompareTo(node.data) < 0)
+				{
+					node = node.left;
+				}
+				else if (data.CompareTo(node.data) > 0)
+				{
+					node = node.right;
+				}
+				else
+				{
+					return true;
+				}
+			}
+			return false;
+
+		}
+
+		public void PrintTree()
+		{
+			if (root != null)
+			{
+				root.PrintData();
+			}
+		}
+	}
+}
---
+++ BST_Recursion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Recursion
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BST bst = new BST();
+			Random rand = new Random();
+			for (int i = 0; i < 10; i++)
+			{
+				bst.InsertRecursively(rand.Next(200));
+			}
+			Console.WriteLine("Tree:");
+			bst.PrintTree();
+
+			Console.ReadKey();
+		}
+	}
+}
